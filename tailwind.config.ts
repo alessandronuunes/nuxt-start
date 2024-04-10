3183e3dbb32ae307d1a5c86a2883d35a8b21b52f
Willy-Bueno <willybueno090@gmail.com>
2024-04-10 17:29:29 -0300

    chore: update eslint config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: ['./src/**/*.{js,ts,vue}']
+    content: ["./src/**/*.{js,ts,vue}"]
 }
 
 export default config
