99bb33488da0cc029ac47d8d1e080b5ceea844a2
Willy-Bueno <willybueno090@gmail.com>
2024-04-10 22:52:59 -0300

    chore: add primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
+import path from "node:path"
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
     srcDir: "src",
-    modules: ["@nuxtjs/tailwindcss"]
+    modules: ["@nuxtjs/tailwindcss", "nuxt-primevue"],
+    primevue: {
+        options: {
+            unstyled: true
+        },
+        importPT: { from: path.resolve(__dirname, "src/presets/lara/") } // import and apply preset
+    }
 })
